7cd63c9c0a235d3b6140acb06204e4f789250c13
CalderMarshall <cmarshall72@gmail.com>
2017-03-19 16:14:40 -0400

    form update

---
+++ js/submit.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+	// send form data to php script with AJAX
+	$('#submit').click(function(event){
+		event.preventDefault();
+		var yourname = $('#yourname').val();
+		var email = $('#email').val();
+		var message = $('#message').val();
+		var formData = {yourname: yourname, email: email, message: message};
+
+		if (yourname == '' || email == '' || message ==''){
+			$('#error-message').fadeIn().html('All fields are required');
+			setTimeout(function(){
+				$('#error-message').fadeOut('slow');
+			}, 4000);
+		}
+		else {
+			$('#error-message').html('');
+			$.ajax({
+				type: 'POST',
+				url: '../mail.php',
+				data: formData,
+				success: function(response){
+					$('input[type=text], input[type=email], textarea').val('');
+					// if (typeof $('.g-recaptcha') != 'undefined') {
+					// 	grecaptcha.reset();
+					// }
+					$('#success-message').fadeIn().html(response);
+					setTimeout(function(){
+						$('#success-message').fadeOut('slow');
+					}, 5000);
+
+				}
+			});
+
+		}
+		return false;
+	});
+});
