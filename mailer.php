3230a18a314aff153414742f59d0478ecc64599b
CalderMarshall <cmarshall72@gmail.com>
2017-03-27 16:48:28 -0400

    mail should work

--- mailer.php
+++ mailer.php
@@ -18,7 +18,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "cmarshalls197@outlook.com";
+        $recipient = "cmarshall72@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
